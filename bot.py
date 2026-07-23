3caf133f634d5f7b0fd10572ecfc3dc7613308e6
sharifikasra303-alt <sharifikasra303@gmail.com>
2026-07-23 13:27:07 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,48 +1,46 @@
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Application, CommandHandler, ContextTypes, CallbackQueryHandler
+from telegram import Update, ReplyKeyboardMarkup
+from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes, filters
 
 TOKEN = "8674292035:AAHRlHUZlQJZHFcPuFwWQ8borwZbFzmzeaA"
 
+keyboard = [
+    ["🎮 شروع دوئل", "💰 موجودی"],
+    ["👥 دعوت دوستان", "🪙 خرید سکه"],
+    ["💸 برداشت وجه"]
+]
+
+reply_markup = ReplyKeyboardMarkup(
+    keyboard,
+    resize_keyboard=True
+)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
-        "سلام 👋\n"
-        "ربات سنگ کاغذ قیچی آماده است!\n\n"
-        "برای شروع بازی بزن:\n"
-        "/game"
-    )
-
-
-async def game(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    keyboard = [
-        [
-            InlineKeyboardButton("🪨 سنگ", callback_data="سنگ"),
-            InlineKeyboardButton("📄 کاغذ", callback_data="کاغذ"),
-            InlineKeyboardButton("✂️ قیچی", callback_data="قیچی")
-        ]
-    ]
-
-    reply_markup = InlineKeyboardMarkup(keyboard)
-
-    await update.message.reply_text(
-        "یکی را انتخاب کن:",
+        "🎮 به ربات سنگ کاغذ قیچی خوش اومدی!",
         reply_markup=reply_markup
     )
 
+async def buttons(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text
 
-async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    query = update.callback_query
-    await query.answer()
+    if text == "💰 موجودی":
+        await update.message.reply_text("💰 موجودی شما: 0 سکه")
 
-    await query.edit_message_text(
-        f"انتخاب تو: {query.data}"
-    )
+    elif text == "🪙 خرید سکه":
+        await update.message.reply_text("🪙 بخش خرید سکه هنوز ساخته نشده.")
 
+    elif text == "💸 برداشت وجه":
+        await update.message.reply_text("💸 بخش برداشت هنوز ساخته نشده.")
+
+    elif text == "👥 دعوت دوستان":
+        await update.message.reply_text("👥 لینک دعوت شما بعداً نمایش داده می‌شود.")
+
+    elif text == "🎮 شروع دوئل":
+        await update.message.reply_text("🎮 این بخش در مرحله بعد ساخته می‌شود.")
 
 app = Application.builder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
-app.add_handler(CommandHandler("game", game))
-app.add_handler(CallbackQueryHandler(button))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, buttons))
 
 app.run_polling()

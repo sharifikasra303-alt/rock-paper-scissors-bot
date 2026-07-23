0728c7772668550c582bbcb7e5df71988e76136e
sharifikasra303-alt <sharifikasra303@gmail.com>
2026-07-23 14:01:52 +0330

    Update database.py

--- database.py
+++ database.py
@@ -1,35 +1,54 @@
 import sqlite3
 
-conn = sqlite3.connect("users.db", check_same_thread=False)
-cursor = conn.cursor()
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS users (
-    user_id INTEGER PRIMARY KEY,
-    balance INTEGER DEFAULT 0
-)
-""")
-
-conn.commit()
+DB_NAME = "database.db"
 
 
-def add_user(user_id):
-    cursor.execute(
-        "INSERT OR IGNORE INTO users (user_id, balance) VALUES (?, ?)",
-        (user_id, 0)
+def connect():
+    return sqlite3.connect(DB_NAME)
+
+
+def create_table():
+    conn = connect()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        user_id INTEGER PRIMARY KEY,
+        first_name TEXT,
+        username TEXT,
+        balance INTEGER DEFAULT 0
     )
+    """)
+
     conn.commit()
+    conn.close()
+
+
+def add_user(user_id, first_name, username):
+    conn = connect()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    INSERT OR IGNORE INTO users
+    (user_id, first_name, username)
+    VALUES (?, ?, ?)
+    """, (user_id, first_name, username))
+
+    conn.commit()
+    conn.close()
 
 
 def get_balance(user_id):
-    add_user(user_id)
+    conn = connect()
+    cursor = conn.cursor()
 
     cursor.execute(
-        "SELECT balance FROM users WHERE user_id = ?",
+        "SELECT balance FROM users WHERE user_id=?",
         (user_id,)
     )
 
     result = cursor.fetchone()
+    conn.close()
 
     if result:
         return result[0]
@@ -37,23 +56,30 @@ def get_balance(user_id):
     return 0
 
 
-def add_balance(user_id, amount):
-    add_user(user_id)
+def update_balance(user_id, amount):
+    conn = connect()
+    cursor = conn.cursor()
 
-    cursor.execute(
-        "UPDATE users SET balance = balance + ? WHERE user_id = ?",
-        (amount, user_id)
-    )
-
-    conn.commit()
-
-
-def set_balance(user_id, amount):
-    add_user(user_id)
-
-    cursor.execute(
-        "UPDATE users SET balance = balance + ? WHERE user_id = ?",
-        (amount, user_id)
-    )
+    cursor.execute("""
+    UPDATE users
+    SET balance = balance + ?
+    WHERE user_id=?
+    """, (amount, user_id))
 
     conn.commit()
+    conn.close()
+
+
+def get_all_users():
+    conn = connect()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    SELECT user_id, first_name, username, balance
+    FROM users
+    """)
+
+    users = cursor.fetchall()
+    conn.close()
+
+    return users

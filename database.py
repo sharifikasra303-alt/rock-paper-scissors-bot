9b558085c6aff96962f4caf8007a1bbbf0f0d672
sharifikasra303-alt <sharifikasra303@gmail.com>
2026-07-23 13:52:17 +0330

    Update database.py

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ conn = sqlite3.connect("users.db", check_same_thread=False)
 cursor = conn.cursor()
 
 cursor.execute("""
-CREATE TABLE IF NOT EXISTS users(
+CREATE TABLE IF NOT EXISTS users (
     user_id INTEGER PRIMARY KEY,
     balance INTEGER DEFAULT 0
 )
@@ -15,7 +15,7 @@ conn.commit()
 
 def add_user(user_id):
     cursor.execute(
-        "INSERT OR IGNORE INTO users(user_id, balance) VALUES(?, ?)",
+        "INSERT OR IGNORE INTO users (user_id, balance) VALUES (?, ?)",
         (user_id, 0)
     )
     conn.commit()
@@ -23,17 +23,37 @@ def add_user(user_id):
 
 def get_balance(user_id):
     add_user(user_id)
+
     cursor.execute(
-        "SELECT balance FROM users WHERE user_id=?",
+        "SELECT balance FROM users WHERE user_id = ?",
         (user_id,)
     )
-    return cursor.fetchone()[0]
+
+    result = cursor.fetchone()
+
+    if result:
+        return result[0]
+
+    return 0
 
 
 def add_balance(user_id, amount):
     add_user(user_id)
+
     cursor.execute(
-        "UPDATE users SET balance = balance + ? WHERE user_id=?",
+        "UPDATE users SET balance = balance + ? WHERE user_id = ?",
         (amount, user_id)
     )
+
+    conn.commit()
+
+
+def set_balance(user_id, amount):
+    add_user(user_id)
+
+    cursor.execute(
+        "UPDATE users SET balance = balance + ? WHERE user_id = ?",
+        (amount, user_id)
+    )
+
     conn.commit()

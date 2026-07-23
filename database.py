ff2c64455322ed8ebc5a1663078e86e1bf18ddf1
sharifikasra303-alt <sharifikasra303@gmail.com>
2026-07-23 13:31:56 +0330

    Create database.py

---
+++ database.py
@@ -0,0 +1,39 @@
+import sqlite3
+
+conn = sqlite3.connect("users.db", check_same_thread=False)
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users(
+    user_id INTEGER PRIMARY KEY,
+    balance INTEGER DEFAULT 0
+)
+""")
+
+conn.commit()
+
+
+def add_user(user_id):
+    cursor.execute(
+        "INSERT OR IGNORE INTO users(user_id, balance) VALUES(?, ?)",
+        (user_id, 0)
+    )
+    conn.commit()
+
+
+def get_balance(user_id):
+    add_user(user_id)
+    cursor.execute(
+        "SELECT balance FROM users WHERE user_id=?",
+        (user_id,)
+    )
+    return cursor.fetchone()[0]
+
+
+def add_balance(user_id, amount):
+    add_user(user_id)
+    cursor.execute(
+        "UPDATE users SET balance = balance + ? WHERE user_id=?",
+        (amount, user_id)
+    )
+    conn.commit()
